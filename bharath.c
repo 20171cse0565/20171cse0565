6f4a6d80ef420f608afcfa9cce940a07d6bb24df
20171cse0565 <60249896+20171cse0565@users.noreply.github.com>
2020-01-25 09:02:08 +0530

    Add files via upload

---
+++ bharath.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,res;
+ a=2,b=3;
+ printf("the value of a before increment is %d\n",a);
+ printf("the value of b before increment is %d\n",b);
+ 
+ res=a+++b; //post increment of a
+
+ printf("the output of result is %d\n",res);
+ printf("the value of a after increment is %d\n",a);
+ printf("the value of b after increment is %d\n",b);
+
+
+}
+ 
